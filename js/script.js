9acbdda115e401c6f5b2222a07a313a5b62fb03a
dburgess <dpburgess88@gmail.com>
2019-07-29 21:33:47 -0400

    added select option menu functionality
    Add behavior based on the user selecting either of the javscript based options or selecting the original option, which will clear the color options.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,60 @@
 const $designOption = $('#design');
 const $colorOption = $('#color');
+const jsPunsClr = ['Cornflower Blue', 'Dark Slate Grey', 'Gold'];
+const loveJsClr = ['Tomato', 'Steel Blue', 'Dim Grey'];
 
-$( document ).ready(function() {
+$(document).ready(function() {
     $('#name').focus()
     $('#other-title').hide();
 
     clearOptions();
+
+    $designOption.change(function() {
+        // add a function here that clears the previous options
+        clearOptions();
+        if ($designOption.val() === 'js puns') {
+            jsPuns();
+        } else if ($designOption.val() === 'heart js') {
+            loveJS();
+        }
+    });
 });
 
-// Done // remove color options in the Color drop down from the outset and the drop down says "Please select a T-shirt theme"
-// when design "Theme - JS Puns" is selected then show colors "Cornflower Blue," "Dark Slate Grey," and "Gold."
-//  when design "Theme - I ♥ JS" is selected then show colors "Tomato," "Steel Blue," and "Dim Grey."
-// when a new theme is selected the color field and drop down menu update
-// if the use clears the design list, then clear the color list
-
-
 const clearOptions = () => {
-    if ($designOption.val() === 'Select Theme') {
-        $colorOption.children().each(function(index, item) {
-            item.remove();
-        })
+    $colorOption.children().each(function(index, item) {
+        item.remove();
+    })
     const $option = `<option>Please select a T-shirt theme</option>`;
     $colorOption.append($option);
-    }
 };
 
-const chooseDesign = () => {
+/*$designOption.change(function() {
+    if ($designOption.val() === 'js puns') {
+        jsPuns();
+    } else if ($designOption.val() === 'heart js') {
+        loveJS();
+    }
+});*/
+/*const chooseDesign = () => {
+    if ($designOption.val() === 'js puns') {
+        jsPuns();
+    } else if ($designOption.val() === 'heart js') {
+        loveJS();
+    }
+};*/
 
+const addOptions = array => {
+    //$colorOption.children().eq(0).remove();
+    $.each(array, function(index, value){
+        let $element = `<option>${value}</option>`;
+        $colorOption.append($element);
+    })
+};
+
+const loveJS = () => {
+    addOptions(loveJsClr);
+};
+
+const jsPuns = () => {
+    addOptions(jsPunsClr);
 };
